e70573a3df19d8e9994b638319e372e22a6af4cc
SciBourne <bourne-sci-hack@yandex.ru>
2024-07-05 01:55:37 +0300

    [ INIT ] Add entry point

---
+++ client/src/app/main.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { RouterProvider } from "react-router-dom"
+
+import "./styles/index.css"
+import router from "./router.tsx"
+
+
+
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+)
+
+
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={ router } />
+  </React.StrictMode>
+)
