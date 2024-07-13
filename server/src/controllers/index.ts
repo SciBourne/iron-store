6f7d9731b5f10b3c509733538561e46965668c62
SciBourne <bourne-sci-hack@yandex.ru>
2024-07-14 00:52:45 +0300

    [ INIT ] Add export controllers

---
+++ server/src/controllers/index.ts
@@ -0,0 +1,16 @@
+import getServerStatus from "./root"
+
+import getCatalog from "./catalog"
+import getProducts from "./products"
+import { getRecomended } from "./recomended"
+
+
+
+
+export {
+  getServerStatus,
+
+  getCatalog,
+  getProducts,
+  getRecomended
+}
