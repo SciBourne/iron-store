f409b28d5a7a250293eb17dc467986acfce1a717
SciBourne <bourne-sci-hack@yandex.ru>
2024-07-14 00:16:19 +0300

    [ FEAT ] Extend script for more collection

--- data/db-bootstrap.js
+++ data/db-bootstrap.js
@@ -1,9 +1,25 @@
 print("\n\nCreate test database: iron_store\n")
 
-const db = connect("mongodb://localhost/iron_store")
-const collectionNames = fs.readdirSync("data/products")
 
-var collections = []
+
+
+const ROOT_DIR = "data"
+const db = connect("mongodb://localhost/iron_store")
+
+
+const collections = [
+  {
+    name: "products",
+    fileNames: fs.readdirSync(`${ROOT_DIR}/products`),
+    fileContents: []
+  },
+
+  {
+    name: "recomended",
+    fileNames: fs.readdirSync(`${ROOT_DIR}/recomended`),
+    fileContents: []
+  }
+]
 
 
 
@@ -11,19 +27,32 @@ var collections = []
 print("Source data:")
 
 
-collectionNames.forEach(
-  (name, i) => {
-    collections.push(fs.readFileSync(`data/products/${name}`, "utf-8"))
-    print(`(${i+1}) data/products/${name}`)
+collections.forEach(
+  (collection) => {
+    collection.fileNames.forEach(
+      (fileName, i) => {
+        collection.fileContents.push(
+          fs.readFileSync(`${ROOT_DIR}/${collection.name}/${fileName}`, "utf-8")
+        )
+
+        print(`(${i+1}) ${ROOT_DIR}/${collection.name}/${fileName}`)
+      }
+    )
   }
 )
 
 
 collections.forEach(
-  (coll) => {
-    db.products.insertMany(EJSON.deserialize(JSON.parse(coll)))
+  (collection) => {
+    collection.fileContents.forEach(
+      (content) => {
+        db[collection.name].insertMany(EJSON.deserialize(JSON.parse(content)))
+      }
+    )
   }
 )
+
+
 
 
 print("\n[ ok ]: The bootstrap of the test database is complete\n\n")
