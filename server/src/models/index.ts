14c8429232eb60f49f4bf4c32309c52f99171699
SciBourne <bourne-sci-hack@yandex.ru>
2024-07-14 00:54:31 +0300

    [ INIT ] Add export data models

---
+++ server/src/models/index.ts
@@ -0,0 +1,11 @@
+import CategoryName from "./category"
+import { Product, VisibilityMask } from "./product"
+import { RecomendedProduct } from "./recomended"
+
+
+export {
+  CategoryName,
+  Product,
+  RecomendedProduct,
+  VisibilityMask
+}
