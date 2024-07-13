208801eed454d29b5c1a10941cd2458fb24e0382
SciBourne <bourne-sci-hack@yandex.ru>
2024-07-14 00:54:52 +0300

    [ INIT ] Add category model

---
+++ server/src/models/category.ts
@@ -0,0 +1,15 @@
+enum CategoryName {
+  cases         = "cases",
+  coolers       = "coolers",
+  memory        = "memory",
+  motherboards  = "motherboards",
+  powersupplies = "powersupplies",
+  processors    = "processors",
+  storages      = "storage",
+  videocards    = "videocards"
+}
+
+
+
+
+export default CategoryName
