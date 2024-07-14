83177ab3b270d0a4a57a3cd8b825a8fc3a5e3911
SciBourne <bourne-sci-hack@yandex.ru>
2024-07-14 04:43:23 +0300

    [ REFACTOR ] Rework configs

--- client/src/config/index.ts
+++ client/src/config/index.ts
@@ -1,6 +1,11 @@
-const ROOT_PRODUCT_IMAGE: string = "/img/products"
+import { ROOT_PRODUCT_IMAGE } from "./public"
+import { SRV_HOST, SRV_PORT, SRV_ENTRY_POINT } from "./net"
 
 
+export {
+  ROOT_PRODUCT_IMAGE,
 
-
-export { ROOT_PRODUCT_IMAGE }
+  SRV_HOST,
+  SRV_PORT,
+  SRV_ENTRY_POINT
+}
