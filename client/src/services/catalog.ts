b8cc5e6d7e94cc5ee3176bcfde1228dd0a800005
SciBourne <bourne-sci-hack@yandex.ru>
2024-07-15 16:04:38 +0300

    [ FEAT ] Add service for get product list

--- client/src/services/catalog.ts
+++ client/src/services/catalog.ts
@@ -4,6 +4,29 @@ import { SRV_ENTRY_POINT, SRV_HOST, SRV_PORT } from "../config"
 
 
 
+function updateProductList(categoryName: string, updateList: Function) {
+  const baseURL: string = `http://${SRV_HOST}:${SRV_PORT}${SRV_ENTRY_POINT}`
+  const endPoint: string = `/catalog/${categoryName}`
+
+  const query: { [prop: string]: number } = {
+    description: 0
+  }
+
+  axios.get(`${baseURL}${endPoint}`, { params: query })
+          .then(
+            (response) => {
+              updateList(response.data)
+            }
+          )
+          .catch(
+            (error) => {
+              console.log(error)
+            }
+          )
+}
+
+
+
 function updateRecomendedList(updateList: Function) {
   const baseURL: string = `http://${SRV_HOST}:${SRV_PORT}${SRV_ENTRY_POINT}`
   const endPoint: string = "/catalog/recomended"
@@ -13,15 +36,19 @@ function updateRecomendedList(updateList: Function) {
   }
 
   axios.get(`${baseURL}${endPoint}`, { params: query })
-          .then((response) => {
-            updateList(response.data)
-          })
-          .catch((error) => {
-            console.log(error)
-          })
+          .then(
+            (response) => {
+              updateList(response.data)
+            }
+          )
+          .catch(
+            (error) => {
+              console.log(error)
+            }
+          )
 }
 
 
 
 
-export { updateRecomendedList }
+export { updateProductList, updateRecomendedList }
